df4a3daf8d8f435fe96e147f015ee7c3e87f6df3
hackeru <hackeru@hackeru3.local>
2018-03-12 20:58:48 +0200

    love

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  name1 = "Moshe";
+  name2 = "Masha";
+  get score() {
+    var concat = (this.name1 + this.name2).toLowerCase(); //ts is even more strict than java woot?!?!?
+    //concat = "MosheMasha"
+    var sum = 0;
+    var arr = concat.split(""); //->['M','o','s','h','e','M','a','s','m','a']
 
-  constructor(public navCtrl: NavController) {
+    arr.forEach(letter => {
+      sum += letter.charCodeAt(0);
+    });
 
+    return sum % 101;
   }
 
+  constructor(public navCtrl: NavController) {}
 }
